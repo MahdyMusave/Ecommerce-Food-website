d79e9e82dd13f38b616a216e7db2e31df28d4e62
MahdyMusave <s.mosave13@gmail.com>
2024-07-25 14:29:51 +0200

    add router-dom , toggle navbar & risponse-mobile

--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -6,7 +6,7 @@ import MobileNav from '../../MobileNav';
     <div className='border-b-2 border-b-orange-500 py-6'>
         <div className='container '>
           <Link to="/" className='text-3xl font-bold tracking-tight text-orange-500'>
-            MahdyMusave.com
+            MahdyMusave .com
           </Link>
           <div className='md:hidden'>
              <MobileNav />
