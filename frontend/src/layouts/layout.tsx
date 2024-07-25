ba5ecf69ccc7a7274691201c9d5d29e2d38c373d
MahdyMusave <s.mosave13@gmail.com>
2024-07-25 16:37:52 +0200

    add login button for navbar

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -9,12 +9,12 @@ const Layout=({children}:Props)=>{
     return(
     
        <>
-            <div className=" flex flex-col min-h-screen">
+            <div className=" flex flex-col min-h-screen border border-red-500 w-screen">
 
                  
                     <Header />
                     <div className="container mx-auto flex-1 py-10 text-slate-500">
-                        {children}
+                      {children}
                     </div>
             </div>
        </>
