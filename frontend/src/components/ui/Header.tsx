37a67d4d5a8b31ceaf7c9e0b5ebe2c26de4e2845
MahdyMusave <s.mosave13@gmail.com>
2024-07-25 14:22:37 +0200

    add router and risponse_mobile & toggle navbar

--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from "react-router-dom";
+import MobileNav from '../../MobileNav';
  const Header = () => {
   return (
     <div className='border-b-2 border-b-orange-500 py-6'>
@@ -7,7 +8,9 @@ import {Link} from "react-router-dom";
           <Link to="/" className='text-3xl font-bold tracking-tight text-orange-500'>
             MahdyMusave.com
           </Link>
-
+          <div className='md:hidden'>
+             <MobileNav />
+          </div>
         </div>
     </div>
   )
